4066d8dd4f0bcc2a7b857ef8c8493e7edd0cbd8a
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-03 18:50:51 +0300

    Add test for creating new squad instance

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,20 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Squad {
+    private int SquadId;
+    private String squadName;
+    private int squadSize;
+    private String cause;
+
+    public Squad(String name, int size, String cause) {
+        squadName = name;
+        squadSize = size;
+        this.cause = cause;
+    }
+
+    public static Squad setUpNewSquad() {
+        return new Squad("Warriors", 5, "Fight corruption");
+    }
 }
--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -2,16 +2,22 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SquadTest {
-
+Squad squad=new Squad("Warriors", 5, "Fight corruption");
     @Before
     public void setUp() throws Exception {
     }
 
     @After
     public void tearDown() throws Exception {
+    }
+    @Test
+    public void newSquad_instantiatesCorrectly_true(){
+        Squad newSquad = Squad.setUpNewSquad();
+        assertTrue(newSquad instanceof Squad);
     }
 }
