4e1cd639d13f7273aafb927bee4cb6bf37092000
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-03 19:21:28 +0300

    Add hero form and its route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,11 +9,15 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-
+//Add root route
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
-
+//Add hero form route
+        get("/hero-form", (req, re) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
